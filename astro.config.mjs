972980e453f6d518f2ecc11a4c5d34762f0992ac
@byongtop <byong.top@gmail.com>
2026-03-05 18:11:29 +1100

    Update: Archive & Visual optimization (周四 2026/03 18:11:29)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   site: 'https://roseisrose.com.au', // 确保这里填写了你的预览或正式域名
-  // Vite 插件配置，用于支持 Tailwind CSS v4
-=======
-  // Vite 插件配置，用于支持 Tailwind CSS v4  
->>>>>>> bf915a2cf3eff50b7a755a97649afe06cc59fde7
   vite: {
     plugins: [tailwindcss()],
   },
